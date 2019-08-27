8493be18f629658dca70c115ae41219f3f971294
ttuom1nen <ttuominen@outlook.com>
2019-08-27 22:29:01 +0300

    Initial commit

---
+++ src/components/PostForm.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+class PostForm extends Component {
+  state = {
+    title: "",
+    body: ""
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Add Post</h1>
+        <form>
+          <div>
+            <label>Title:</label>
+            <br />
+            <input
+              type="text"
+              name="title"
+              onChange={this.onChange}
+              value={this.state.title}
+            />
+          </div>
+          <br />
+          <div>
+            <label>Body:</label>
+            <br />
+            <textarea
+              type="text"
+              name="body"
+              onChange={this.onChange}
+              value={this.state.body}
+            />
+          </div>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default PostForm;
